87e9d81e017f7f2ac7120df1ec659534080547ce
Nibbles <siliconrob@siliconheaven.net>
2017-06-18 14:44:37 -0700

    Region us-west-2a is publicly us-west-2
    The a is silent

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -4,7 +4,7 @@
 export IMAGE_NAME=ecsdemo-redisgeo
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=us-west-2a
+export AWS_DEFAULT_REGION=us-west-2
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=52.26.25.243
 
